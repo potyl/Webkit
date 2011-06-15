b49d4c70a28c63cdf43b8cc0cca75cd27a2932bc
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-06-15 07:48:33 +0200

    Add a super nany (TM) mode

--- nanny.pl
+++ nanny.pl
@@ -23,11 +23,16 @@ use warnings;
 use Glib qw(TRUE FALSE);
 use Gtk2 -init;
 use WWW::WebKit;
+use Getopt::Long qw(:config auto_help);
+use Pod::Usage;
 use Data::Dumper;
 use URI;
 
 
 sub main {
+    GetOptions(
+        's|super' => \my $super,
+    ) or podusage(1);
     my ($url) = @ARGV;
     $url ||= 'http://localhost:3001/';
     my $allowed_host_port = get_host_port($url);
@@ -37,23 +42,40 @@ sub main {
     $window->signal_connect(destroy => sub { Gtk2->main_quit() });
 
     my $view = WWW::WebKit::WebView->new();
-    
-    # Add a callback to monitor where each URI will go and reject the URI if the
-    # location differs from the original website.
-    # This only blocks, iframes and clicked links. Javascript and CSS are not blocked.
-    $view->signal_connect("navigation-policy-decision-requested" => sub {
-        my ($view, $frame, $request, $action, $decision) = @_;
-        
-        my $host_port = get_host_port($request->get_uri);
-        
-        # We allow browsing the same site
-        return FALSE if $host_port eq $allowed_host_port;
-        
-        # We block the access to an external site
-        print "Block access to $host_port\n";
-        $decision->ignore();
-        return TRUE;
-    });
+
+    if ($super) {
+        print "Super nanny activated\n";
+        $view->signal_connect("resource-request-starting" => sub {
+            my ($view, $frame, $resource, $request, $response) = @_;
+
+            my $host_port = get_host_port($request->get_uri);
+            return if $host_port eq $allowed_host_port;
+
+            # Block the request if it goes outside, we block by setting the URI
+            # to 'about:blank'
+            print "Block access to $host_port\n";
+            $request->set_uri('about:blank');
+        });
+    }
+    else {
+        print "Nanny activated\n";
+        # Add a callback to monitor where each URI will go and reject the URI if the
+        # location differs from the original website.
+        # This only blocks, iframes and clicked links. Javascript and CSS are not blocked.
+        $view->signal_connect("navigation-policy-decision-requested" => sub {
+            my ($view, $frame, $request, $action, $decision) = @_;
+
+            my $host_port = get_host_port($request->get_uri);
+
+            # We allow browsing the same site
+            return FALSE if $host_port eq $allowed_host_port;
+
+            # We block the access to an external site
+            print "Block access to $host_port\n";
+            $decision->ignore();
+            return TRUE;
+        });
+    }
 
     # Pack the widgets together
     my $scrolls = Gtk2::ScrolledWindow->new();
