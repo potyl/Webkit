e31137360947ede863bec68d244c05f81637bbbf
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-06-19 19:25:48 +0200

    Add gir.pl

---
+++ gir.pl
@@ -0,0 +1,32 @@
+#!perl
+
+use strict;
+use warnings;
+use Glib::Object::Introspection;
+
+Glib::Object::Introspection->setup(
+  basename => 'Gtk',
+  version => '2.0',
+  package => 'Gtk2');
+
+Glib::Object::Introspection->setup(
+  basename => 'WebKit',
+  version => '1.0',
+  package => 'Gtk2::WebKit');
+
+Gtk2::init(0, []);
+
+my $view = Gtk2::WebKit::WebView->new;
+
+my $sw = Gtk2::ScrolledWindow->new;
+$sw->add($view);
+
+my $win = Gtk2::Window->new ('toplevel');
+$win->set_default_size(800, 600);
+$win->signal_connect(destroy => sub { Gtk2->main_quit });
+$win->add($sw);
+$win->show_all;
+
+$view->load_uri( $ARGV[0] // 'http://perldition.org' );
+
+Gtk2->main;
