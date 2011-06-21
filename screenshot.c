8f343000699cdd1b0417869bbbe7f7a11d089312
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-06-21 23:19:36 +0200

    Cleanup

--- screenshot.c
+++ screenshot.c
@@ -10,7 +10,7 @@ load_status_cb (GObject* object, GParamSpec* pspec, gpointer data);
 
 static void
 load_status_cb (GObject* object, GParamSpec* pspec, gpointer data) {
-    GMainLoop *loop = (GMainLoop *) data;
+    const gchar *filename = (const gchar *) data;
 
     WebKitWebView *web_view = WEBKIT_WEB_VIEW(object);
     WebKitLoadStatus status = webkit_web_view_get_load_status(web_view);
@@ -20,8 +20,6 @@ load_status_cb (GObject* object, GParamSpec* pspec, gpointer data) {
 
     GtkAllocation allocation;
     gtk_widget_get_allocation(GTK_WIDGET(web_view), &allocation);
-
-    const char *filename = "a.pdf";
     cairo_surface_t *surface = cairo_pdf_surface_create(
         filename,
         1.0 * allocation.width,
@@ -33,40 +31,32 @@ load_status_cb (GObject* object, GParamSpec* pspec, gpointer data) {
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
 
-    g_main_loop_quit(loop);
+    gtk_main_quit();
 }
 
 
 int
-main (int argc, char* argv[]) {
-    const char *uri;
-    WebKitWebView* web_view;
-    GMainLoop* loop;
-
+main (int argc, gchar* argv[]) {
     gtk_init(&argc, &argv);
 
-    if (argc == 1) {
-        printf("Usage: URI\n");
+    if (argc < 2) {
+        printf("Usage: URI [filename]\n");
         return 1;
     }
-    uri = argv[1];
+    const gchar *uri = argv[1];
+    const gchar *filename = argc > 2 ? argv[2] : "a.pdf";
 
     if (!g_thread_supported()) {g_thread_init(NULL);}
 
-    loop = g_main_loop_new(NULL, TRUE);
-
-    web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
-    g_object_ref_sink(G_OBJECT(web_view));
-
-    g_signal_connect(web_view, "notify::load-status", G_CALLBACK(load_status_cb), loop);
-    webkit_web_view_load_uri(web_view, uri);
+    WebKitWebView *web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    g_signal_connect(web_view, "notify::load-status", G_CALLBACK(load_status_cb), (gpointer) filename);
 
     GtkWidget *offscren = gtk_offscreen_window_new();
     gtk_container_add(GTK_CONTAINER(offscren), GTK_WIDGET(web_view));
     gtk_widget_show_all(offscren);
 
-    g_main_loop_run(loop);
-    g_object_unref(web_view);
+    webkit_web_view_load_uri(web_view, uri);
+    gtk_main();
 
     return 0;
 }
