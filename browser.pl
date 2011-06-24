56bc0132fcaf6f84fbbd4394bb9607097445a912
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-06-24 07:46:47 +0200

    Using an OffscreenWindow and Cairo::GObject

--- browser.pl
+++ browser.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 use Glib::Object::Introspection;
 
 Glib::Object::Introspection->setup(
@@ -16,35 +18,30 @@ Glib::Object::Introspection->setup(
   version  => '3.0',
   package  => 'WebKit'
 );
-use Cairo;
+use Cairo::GObject;
 
 Gtk3::init(0, []);
 my ($url) = @ARGV;
 $url ||= 'http://localhost:3001/';
 
-
 my $view = WebKit::WebView->new();
-
-my $scrolls = Gtk3::ScrolledWindow->new();
-$scrolls->add($view);
-
-my $win = Gtk3::Window->new ('toplevel');
-$win->set_default_size(800, 600);
-$win->signal_connect(destroy => sub { Gtk3->main_quit(); });
-$win->add($scrolls);
-$win->show_all();
-
 $view->signal_connect('notify::load-status' => sub {
-    my $uri = $view->get_uri or return;
-    return unless $view->get_load_status eq 'finished';
+    return unless $view->get_uri and ($view->get_load_status eq 'finished');
 
+#    my $allocation = $view->get_allocation;
+#    my ($width, $height) = ($allocation->{width}, $allocation->{height});
     my ($width, $height) = ($view->get_allocated_width, $view->get_allocated_height);
-
+    print "size = $width x $height\n";
     my $surface = Cairo::PdfSurface->create("a.pdf", 1.0 * $width, 1.0 * $height);
     my $cr = Cairo::Context->create($surface);
     $view->draw($cr);
 });
-
 $view->load_uri($url);
+
+
+my $win = Gtk3::OffscreenWindow->new();
+$win->add($view);
+$win->show_all();
+
 
 Gtk3->main();
