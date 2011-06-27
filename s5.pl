eef7da9eb7908a05b560206572f48bce11274e17
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-06-27 09:05:32 +0200

    Works as expected, the slides are saved in a single PDF file

--- s5.pl
+++ s5.pl
@@ -44,6 +44,7 @@ use Cairo::GObject;
 use constant TRUE  => 1;
 use constant FALSE => 0;
 
+
 sub main {
     Gtk3::init(0, []);
 
@@ -52,9 +53,40 @@ sub main {
     $uri = "file://$uri" if -e $uri;
     $filename ||= "s5.pdf";
 
+
     my $view = WebKit::WebView->new();
 
+    # Introduce some JavaScript helper methods. This methods will communicate
+    # with the Perl script by writting data to the consolse.
+    $view->execute_script(q{
+        function _is_end_of_slides () {
+            var last_slide = (snum == smax - 1) ? true : false;
+            var last_subslide = ( !incrementals[snum] || incpos >= incrementals[snum].length ) ? true : false;
+            var ret = (last_slide && last_subslide) ? true : false ;
+            //console.log("last_slide: " + last_slide + "; last_subslide: " + last_subslide + "; fini: " + ret);
+            //console.log("end? " +  (  (snum == smax - 1) && ( !incrementals[snum] || incpos >= incrementals[snum].length ) ));
+            console.log("s5-end-of-slides: " + ret);
+            return ret;
+        }
 
+        function _next_slide () {
+            //console.log("Next slide (snum:" + snum + "; incpos: " + incpos + ") ?");
+            if (!incrementals[snum] || incpos >= incrementals[snum].length) {
+                go(1);
+            }
+            else {
+                subgo(1);
+            }
+
+            _is_end_of_slides();
+        }
+
+    });
+
+
+    # Start taking screenshot as soon as the document is loaded. Maybe we want
+    # to add an onload callback and to log a message once we're ready. We want
+    # to take a screenshot only when the page is done being rendered.
     my $surface;
     $view->signal_connect('notify::load-status' => sub {
         return unless $view->get_uri and ($view->get_load_status eq 'finished');
@@ -64,34 +96,39 @@ sub main {
         #  (<unknown>:19092): Gtk-CRITICAL **: gtk_widget_draw: assertion `!widget->priv->alloc_needed' failed
         # This seem to happend is there's a newtwork error and we can't download
         # external stuff (e.g. facebook iframe). This timeout seems to help a bit.
-        Glib::Idle->add( sub {
+        Glib::Idle->add(sub {
             $view->execute_script(q{ _is_end_of_slides(); });
         });
     });
     $view->load_uri($uri);
 
 
+    # The JavaScripts communicates with Perl by writting into the console. This
+    # is a hack but this is the best way that I could find so far.
     $view->signal_connect('console-message' => sub {
         my ($widget, $message, $line, $source_id) = @_;
         print "CONSOLE $message at $line $source_id\n";
         my ($end) = ( $message =~ /^s5-end-of-slides: (true|false)$/) or return TRUE;
 
+        # See if we need to create a new PDF or a new page
+        if ($surface) {
+            $surface->show_page();
+        }
+        else {
+            my ($width, $height) = ($view->get_allocated_width, $view->get_allocated_height);
+            $surface = Cairo::PdfSurface->create($filename, $width, $height);
+        }
+
+        # A new slide has been rendered on screen, we save it to the pdf
+        my $cr = Cairo::Context->create($surface);
+        $view->draw($cr);
+
+        # Go to the next slide or stop grabbing screenshots
         if ($end eq 'true') {
+            # No more slides to grab
             Gtk3->main_quit();
         }
         else {
-            # See if we need to create a new PDF or a new page
-            if ($surface) {
-                $surface->show_page();
-            }
-            else {
-                my ($width, $height) = ($view->get_allocated_width, $view->get_allocated_height);
-                $surface = Cairo::PdfSurface->create($filename, $width, $height);
-            }
-
-            # A new slide has been rendered on screen
-            save_as_pdf($surface, $view);
-
             # Go on with the slide
             $view->execute_script(q{ _next_slide(); });
         }
@@ -99,37 +136,9 @@ sub main {
         return TRUE;
     });
 
-    $view->execute_script(q{
-        function _is_end_of_slides () {
-            var last_slide = (snum == smax - 1) ? true : false;
-            var last_subslide = ( !incrementals[snum] || incpos >= incrementals[snum].length ) ? true : false;
-            var ret = (last_slide && last_subslide) ? true : false ;
-            console.log("last_slide: " + last_slide + "; last_subslide: " + last_subslide + "; fini: " + ret);
-            console.log("end? " +  (  (snum == smax - 1) && ( !incrementals[snum] || incpos >= incrementals[snum].length ) ));
-            console.log("s5-end-of-slides: " + ret);
-            return ret;
-        }
-
-        function _next_slide () {
-            console.log("Next slide (snum:" + snum + "; incpos: " + incpos + ") ?");
-            if (!incrementals[snum] || incpos >= incrementals[snum].length) {
-                console.log("go(1)");
-                go(1);
-            }
-            else {
-                console.log("subgo(1)");
-                subgo(1);
-            }
-            
-            console.log("s5-end-of-slides: " + true);
-            _is_end_of_slides();
-        }
-
-    });
-
     my $window = Gtk3::OffscreenWindow->new();
 #    my $window = Gtk3::Window->new('toplevel');
-    $window->set_default_size(600, 400);
+    $window->set_default_size(800, 600);
     $window->add($view);
     $window->show_all();
 
@@ -143,5 +152,6 @@ sub save_as_pdf {
     my $cr = Cairo::Context->create($surface);
     $widget->draw($cr);
 }
+
 
 exit main() unless caller;
