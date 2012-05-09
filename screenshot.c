2671d654c7b064d2b66c94937cb3de9625ef6446
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2012-05-09 22:29:40 +0200

    Work better with Ubuntu 12.04 & webkit 1.8.0

--- screenshot.c
+++ screenshot.c
@@ -2,12 +2,20 @@
 #include <webkit/webkit.h>
 #include <libsoup/soup.h>
 #include <cairo-pdf.h>
+#include <glib/gprintf.h>
+
+
+typedef struct IdleData_ {
+    WebKitWebView *web_view;
+    const char    *filename;
+} IdleData;
 
 
 static void
 save_as_pdf (GtkWidget *widget, const char *filename) {
     GtkAllocation allocation;
 
+    g_printf("Saving PDF to file %s\n", filename);
     gtk_widget_get_allocation(widget, &allocation);
     cairo_surface_t *surface = cairo_pdf_surface_create(
         filename,
@@ -22,6 +30,16 @@ save_as_pdf (GtkWidget *widget, const char *filename) {
 }
 
 
+static gboolean
+idle_cb (gpointer data) {
+    IdleData *idle_data = (IdleData *) data;
+    save_as_pdf(GTK_WIDGET(idle_data->web_view), idle_data->filename);
+    g_free(idle_data);
+    gtk_main_quit();
+    return FALSE;
+}
+
+
 static void
 load_status_cb (GObject* object, GParamSpec* pspec, gpointer data) {
     WebKitWebView *web_view = WEBKIT_WEB_VIEW(object);
@@ -29,10 +47,12 @@ load_status_cb (GObject* object, GParamSpec* pspec, gpointer data) {
     if (status != WEBKIT_LOAD_FINISHED) {
         return;
     }
+    g_printf("Downloaded\n");
 
-    save_as_pdf(GTK_WIDGET(web_view), (const gchar *) data);
-
-    gtk_main_quit();
+    IdleData *idle_data = g_new0(IdleData, 1);
+    idle_data->web_view = web_view;
+    idle_data->filename = (const char*) data;
+    g_timeout_add(2000, idle_cb, (gpointer) idle_data);
 }
 
 
@@ -50,11 +70,13 @@ main (int argc, gchar* argv[]) {
     if (!g_thread_supported()) {g_thread_init(NULL);}
 
     WebKitWebView *web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+
     g_signal_connect(web_view, "notify::load-status", G_CALLBACK(load_status_cb), (gpointer) filename);
 
-    GtkWidget *offscren = gtk_offscreen_window_new();
-    gtk_container_add(GTK_CONTAINER(offscren), GTK_WIDGET(web_view));
-    gtk_widget_show_all(offscren);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 640);
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(web_view));
+    gtk_widget_show_all(window);
 
     webkit_web_view_load_uri(web_view, uri);
     gtk_main();
